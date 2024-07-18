3d1cc14c6a3b64473e233dc7f319fea155332143
ShaneSkare <146023601+ShaneSkare@users.noreply.github.com>
2024-07-18 04:46:16 +0000

    Test, No Changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 
 # flask --app app --debug run
-
+# Test push- Shane
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
