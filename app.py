21349bb1684bff7508547524bfeaa5a0ca7ade87
Carson Mehl <cmehl@csumb.edu>
2024-07-21 22:31:56 -0700

    Carson's second test commit because the first one was weird. added comment line 8 on app.py.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from quiz_info import quiz_info
 # flask --app app --debug run
 # Test push- Shane
 # Carson's test push just making sure it's all good.
+# Carson's second test first one was weird.
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
